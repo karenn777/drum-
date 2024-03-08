315ca730e4c61b0ae8e4de9dd6e939249e1a70f8
Teacher <teacher-19@ah4tjr15jq6w2.78b420.nycboe.org>
2024-03-08 12:34:54 -0500

    “done

---
+++ script.js
@@ -0,0 +1,21 @@
+function drum(e){
+if (e.propertyName !== "transform") return;
+e.target.classlist.remove(playing);
+
+};
+
+function music(e){
+    let soundss {audio[data-key="${e.keyCode}"]
+    if (!audio) return;
+    
+};
+
+
+
+
+  let keys{div[data-key="${e.keyCode}" class="playing"]
+
+};
+    playing{
+        currentTime=0;
+    }
